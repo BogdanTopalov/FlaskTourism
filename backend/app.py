8cec141e75294b4f416c0c5f1d919e84fdf3a5d3
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-01 22:08:31 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,25 @@
+from os import environ
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from db import db
+from routes import routes
+
 
 app = Flask(__name__)
+app.config.from_object(environ.get('CONFIG_OBJECT'))
 
+migrate = Migrate(app, db)
+api = Api(app)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Create initial db instance
+with app.app_context():
+    db.init_app(app)
 
+# Add routes
+[api.add_resource(*route) for route in routes]
 
 if __name__ == '__main__':
     app.run()

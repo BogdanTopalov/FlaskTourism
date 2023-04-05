ab447daf512bf3a76da7975ef89c9bd353ce5636
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-05 22:06:52 +0300

    Create User model

---
+++ backend/models/users.py
@@ -0,0 +1,15 @@
+from db import db
+from models.enums import RoleType
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(
+        db.Enum(RoleType),
+        default=RoleType.regular,
+        nullable=False
+    )

eeee36016a1e175f8ee457422882379db112a5c8
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-18 00:09:19 +0300

    Add hotel model

---
+++ backend/models/hotels.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from db import db
+
+
+class Hotel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    stars = db.Column(db.Integer, nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    price_per_night = db.Column(db.Float, nullable=False)
+    added_on = db.Column(
+        db.DateTime,
+        default=datetime.utcnow(),
+        nullable=False
+    )

fe593ba7961d6505b4c6eefd365aaac9231ca325
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-18 00:08:15 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,12 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-
-from constants import CONFIG
+from app_config import create_app
 from db import db
-from routes import routes
 
-app = Flask(__name__)
-app.config.from_object(CONFIG['CONFIG_OBJECT'])
 
-migrate = Migrate(app, db)
-api = Api(app)
-CORS(app)
+app = create_app('CONFIG_OBJECT')
 
 # Create initial db instance
 with app.app_context():
     db.init_app(app)
-
-# Add routes
-[api.add_resource(*route) for route in routes]
 
 if __name__ == '__main__':
     app.run()

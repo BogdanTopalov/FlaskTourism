965bec43e65ab55a49b8252ee14b7b397bb6f0be
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-18 00:07:55 +0300

    Delete constants.py

--- backend/constants.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values
-
-
-CONFIG = dotenv_values('.env')
-
-DB_USER = CONFIG['DB_USER']
-DB_PASSWORD = CONFIG['DB_PASS']
-DB_NAME = CONFIG['DB_NAME']
-DB_PORT = CONFIG['DB_PORT']

ea25746866b395090b8e9beccab9a91fec96a93e
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-18 00:08:09 +0300

    Update app_config.py

--- backend/app_config.py
+++ backend/app_config.py
@@ -1,14 +1,18 @@
-from constants import DB_USER, DB_NAME, DB_PORT, DB_PASSWORD
+from decouple import config
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from db import db
+from routes import routes
 
 
-# class ProductionConfig:
-#     FLASK_ENV = 'prod'
-#     DEBUG = False
-#     TESTING = False
-#     SQLALCHEMY_DATABASE_URI = (
-#         f'postgresql://{DB_USER}:{DB_PASSWORD}'
-#         f'@localhost:{DB_PORT}/{DB_NAME}'
-#     )
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASS')
+DB_NAME = config('DB_NAME')
+DB_PORT = config('DB_PORT')
+TEST_DB_NAME = config('TEST_DB_NAME')
 
 
 class DevelopmentConfig:
@@ -19,3 +23,25 @@ class DevelopmentConfig:
         f'postgresql://{DB_USER}:{DB_PASSWORD}'
         f'@localhost:{DB_PORT}/{DB_NAME}'
     )
+
+
+class TestingConfig:
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = (
+        f'postgresql://{DB_USER}:{DB_PASSWORD}'
+        f'@localhost:{DB_PORT}/{TEST_DB_NAME}'
+    )
+
+
+def create_app(config_object):
+    app = Flask(__name__)
+    app.config.from_object(config(config_object))
+
+    migrate = Migrate(app, db)
+    api = Api(app)
+    CORS(app)
+
+    [api.add_resource(*route) for route in routes]
+
+    return app

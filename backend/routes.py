d4744e2c031e2c4cda14e496aa62f24910d75139
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-18 19:50:46 +0300

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,1 +1,14 @@
-routes = ()
+from resources.auth import RegisterResource, LoginResource
+from resources.hotels import GetAllHotelsResource, AddHotelResource,\
+    GetHotelResource, UpdateHotelResource, DeleteHotelResource
+
+
+routes = (
+    (RegisterResource, '/register'),
+    (LoginResource, '/login'),
+    (GetAllHotelsResource, '/hotels'),
+    (AddHotelResource, '/hotels/add'),
+    (GetHotelResource, '/hotels/<int:pk>'),
+    (UpdateHotelResource, '/hotels/update/<int:pk>'),
+    (DeleteHotelResource, '/hotels/delete/<int:pk>')
+)

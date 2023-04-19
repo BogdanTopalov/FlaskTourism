31c7808a3863d65392e4cb1ad0c7ccfff9289feb
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-04-19 23:12:40 +0300

    Create reservations.py

---
+++ backend/resources/reservations.py
@@ -0,0 +1,24 @@
+from flask import request
+from flask_restful import Resource
+
+from helpers.decorators import permission_required
+from managers.auth_manager import auth
+from managers.reservation_manager import ReservationManager
+from models.enums import RoleType
+from schemas.response.reservations import ReservationResponseSchema
+
+
+class CreateReservationResource(Resource):
+    @auth.login_required
+    def post(self):
+        data = request.get_json()
+        reservation = ReservationManager.create_reservation(data)
+        return ReservationResponseSchema().dump(reservation)
+
+
+class GetAllReservationsResource(Resource):
+    @auth.login_required
+    @permission_required(RoleType.admin)
+    def get(self):
+        all_reservations = ReservationManager.get_all_reservations()
+        return ReservationResponseSchema(many=True).dump(all_reservations)
